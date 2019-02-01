e30542bfef0b20375b9e2eabdf8d3ce6b65f829b
Dominic Dominguez <dominic.dominguez@pcc.edu>
2019-01-31 19:37:20 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Dominic Dominguez on 01/31/2019
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
